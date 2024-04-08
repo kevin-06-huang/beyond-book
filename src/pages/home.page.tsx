c38b0fea4f2007d05191df532c7ebb5680b7e9f5
Kevin <kevin.06.huang@gmail.com>
2024-04-08 01:10:39 -0700

    feat: initialize project repository

---
+++ src/pages/home.page.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+const HomePage = () => {
+
+  return (
+    <>
+      <section className="bg-ct-blue-600 min-h-screen pt-20">
+        <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
+          <p className="text-3xl font-semibold">
+            Amazing children's book
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
