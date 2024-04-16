6ffb46a5b3395f899b7f87c2efde7a376ae44ad1
Kevin <kevin.06.huang@gmail.com>
2024-04-15 19:32:09 -0700

    feat: style Navbar and build tabs

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,28 @@
+import { NavLink } from "react-router-dom";
 import { Logo } from "./shared/Logo";
+
+const getNavLinkClass = ({ isActive }: { isActive: boolean }) => {
+  return isActive
+    ? "bg-gray-900 hover:bg-gray-700 px-3 py-2 rounded-md text-white"
+    : "hover:bg-gray-700 px-3 py-2 rounded-md text-white";
+};
+
 const NavBar = () => {
   return (
     <nav className="bg-gray-800 p-4 flex justify-between items-center">
-      <Logo />
-      {"Navbar"}
+      <Logo className={"size-24"} />
+      <div className="flex space-x-4">
+        <NavLink to="/" className={getNavLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/library" className={getNavLinkClass}>
+          Library
+        </NavLink>
+        <NavLink to="/about" className={getNavLinkClass}>
+          About Us
+        </NavLink>
+      </div>
+      {/* Other nav items */}
     </nav>
   );
 };
