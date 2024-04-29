257c0b67d94f5bd75dacea46d825b6d240586072
Kevin <kevin.06.huang@gmail.com>
2024-04-28 23:09:38 -0700

    feat: implement genre buttons

--- frontend/src/components/Carousel/GenreButton.tsx
+++ frontend/src/components/Carousel/GenreButton.tsx
@@ -1,14 +1,17 @@
 const GenreButton = ({ genre, src }: { genre: string; src: string }) => {
   return (
-    <button
-      className="bg-blue-500 text-opacity-30 hover:text-opacity-100 text-white font-bold py-2 px-2 rounded inline-flex items-center justify-center m-1 min-w-[150px] min-h-[200px]"
-      style={{
-        backgroundImage: `url(${src})`,
-        backgroundSize: "100% 100%",
-        backgroundPosition: "center",
-      }}
-    >
-      {genre}
+    <button className="relative group bg-blue-500 font-bold rounded inline-flex items-center justify-center m-1 transition ease-in-out duration-300">
+      <span className="absolute z-10 flex items-center justify-center text-opacity-30 group-hover:text-opacity-100 text-white font-bold">
+        {genre}
+      </span>
+      <span
+        className="z-0 group-hover:brightness-50 w-[150px] h-[200px] rounded"
+        style={{
+          backgroundImage: `url(${src})`,
+          backgroundSize: "100% 100%",
+          backgroundPosition: "center",
+        }}
+      />
     </button>
   );
 };
