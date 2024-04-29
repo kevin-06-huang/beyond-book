9b5891c58b5afb7e7c346f5416b53d9c78a713da
Kevin <kevin.06.huang@gmail.com>
2024-04-29 01:01:48 -0700

    fix: debug styling

--- frontend/src/components/Carousel/CustomCarousel.tsx
+++ frontend/src/components/Carousel/CustomCarousel.tsx
@@ -8,11 +8,11 @@ const CustomCarousel = ({ genre }: { genre: string }) => {
   const images: ImageDataType = [];
 
   return (
-    <div className="flex justify-center items-center">
-      <Carousel
-        transition={{ duration: 2 }}
-        className="rounded-xl max-h-[65vh]"
-      >
+    <div
+      className="flex justify-center items-center"
+      style={{ height: "65vh" }}
+    >
+      <Carousel transition={{ duration: 2 }} className="rounded-xl">
         <img
           src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
           alt="image 1"
