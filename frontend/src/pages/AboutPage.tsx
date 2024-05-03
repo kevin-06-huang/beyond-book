4900eda686ea967a5ce07984b52470d496c1bc5e
Kevin <kevin.06.huang@gmail.com>
2024-05-02 21:05:13 -0700

    feat: build about page

--- frontend/src/pages/AboutPage.tsx
+++ frontend/src/pages/AboutPage.tsx
@@ -1,12 +1,26 @@
 import useDocumentTitle from "../hooks/useDocumentTitle";
+import read from "../assets/read.gif";
 
 const AboutPage = () => {
   useDocumentTitle("About Us");
   return (
     <>
-      <section className="bg-ct-blue-600 min-h-screen pt-20">
-        <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-center items-center">
-          <p className="text-3xl font-semibold">ABOUT US</p>
+      <section className="bg-ct-blue-600 px-12 py-12 mt-24 mx-8 bg-white shadow-lg rounded-xl">
+        <div className="mx-auto bg-ct-dark-100 rounded-md flex justify-center items-center">
+          <p className="text-3xl font-semibold">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;At Book Buddy, we
+            believe in the power of storytelling to ignite imaginations and
+            inspire young minds. Our mission is to revolutionize bedtime reading
+            by offering a mobile storytelling app like no other. Powered by
+            Gemini AI, Book Buddy delivers personalized stories tailored to each
+            child's age and preferences, ensuring a magical and immersive
+            experience every time. With interactive features that allow children
+            to shape their own adventures, Book Buddy empowers young readers to
+            become active participants in their storytelling journey. Join us on
+            a quest to unleash the boundless possibilities of imagination, one
+            story at a time.
+          </p>
+          <img src={read} alt={"READ"} />
         </div>
       </section>
     </>
