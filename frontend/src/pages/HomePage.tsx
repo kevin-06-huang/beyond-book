01f0b4a8f0ac175c155eaf25f24c3e05f24b6145
Kevin <kevin.06.huang@gmail.com>
2024-05-02 21:33:09 -0700

    fix: refactor homepage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,27 +1,8 @@
-import { useEffect, useState } from "react";
 import useDocumentTitle from "../hooks/useDocumentTitle";
 import { Carousel } from "../components/Carousel";
 
 const HomePage = () => {
-  const [message, setMessage] = useState("");
   useDocumentTitle("Home");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api/message");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const data = await response.json();
-        setMessage(data.message);
-      } catch (err) {
-        console.error("Error: ", err);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   return (
     <>
